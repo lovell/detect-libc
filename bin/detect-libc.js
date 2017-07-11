3b80606006e70bb68ad4257ea2a32ee01d1eb258
Lovell Fuller <github@lovell.info>
2017-07-11 20:56:18 +0100

    Spawn child process in shell for Windows compat

--- bin/detect-libc.js
+++ bin/detect-libc.js
@@ -5,9 +5,14 @@
 const spawnSync = require('child_process').spawnSync;
 const family = require('../').family;
 
-const spawnOptions = { stdio: 'inherit', env: process.env };
+const spawnOptions = {
+  env: process.env,
+  shell: true,
+  stdio: 'inherit'
+};
+
 if (family) {
-  spawnOptions.env.LIBC = family;
+  spawnOptions.env.LIBC = process.env.LIBC || family;
 }
 
 process.exit(spawnSync(process.argv[2], process.argv.slice(3), spawnOptions).status);
